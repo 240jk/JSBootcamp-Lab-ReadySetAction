8db7cedb70c61406be400fd2c04382ba2af646b6
240jk <jkubis@alunmi.nmu.edu>
2022-11-09 23:12:32 -0500

    initial commit

---
+++ readysetaction.js
@@ -0,0 +1,29 @@
+//create a function named "main"
+function main(){
+
+
+//add the "big" class to the "grow-me" paragraph
+    let bigMe = ;
+
+//remove the "big" class from the "shrink-me" paragraph
+    let removeBig = ;
+
+//find all the <li>'s and log their text content to the console
+    let liTag = document.querySelectorAll("li");
+    console.log(liTag);
+
+//set the href of the link to "https://www.example.com" and update the text to say "somewhere" instead of "nowhere"
+    letChangeHref = document.getElementsByClassName("link") {
+        setAttribute("location.href='https://www.example.com'");
+        
+    }
+//set the "display" CSS property of the "hide-me" paragraph to "none"
+
+
+//set the "display" CSS Property of the "show-me" paragraph to "block"
+
+
+//get the text that the user enters into the "name" input box and use it to set a welcome message in the <h1>, e.g. "Welcome Grant!"
+
+
+}
